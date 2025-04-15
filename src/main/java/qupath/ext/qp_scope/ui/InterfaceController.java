42dba1ad18660c7acbcaf7f50218aec0c26d33bb
MichaelSNelson <85200392+MichaelSNelson@users.noreply.github.com>
2025-04-14 19:18:20 -0500

    Update InterfaceController.java

--- src/main/java/qupath/ext/qp_scope/ui/InterfaceController.java
+++ src/main/java/qupath/ext/qp_scope/ui/InterfaceController.java
@@ -1,10 +1,10 @@
-package qupath.ext.template.ui;
+package qupath.ext.qp_scope.ui;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Spinner;
 import javafx.scene.layout.VBox;
-import qupath.ext.template.DemoExtension;
+import qupath.ext.qp_scope.setup;
 import qupath.fx.dialogs.Dialogs;
 
 import java.io.IOException;
@@ -48,7 +48,7 @@ public class InterfaceController extends VBox {
         // it may be better to present them all to the user in the main extension GUI,
         // binding them to GUI elements, so they are updated when the user interacts with
         // the GUI, and so that the GUI elements are updated if the preference changes
-        integerOptionSpinner.getValueFactory().valueProperty().bindBidirectional(DemoExtension.integerOptionProperty());
+        integerOptionSpinner.getValueFactory().valueProperty().bindBidirectional(setup.integerOptionProperty());
         integerOptionSpinner.getValueFactory().valueProperty().addListener((observableValue, oldValue, newValue) -> {
             Dialogs.showInfoNotification(
                     resources.getString("title"),
