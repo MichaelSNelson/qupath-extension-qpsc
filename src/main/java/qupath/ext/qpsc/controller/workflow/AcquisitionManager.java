26d520eac56e803240e5fa221c91fdc1363d64e7
MichaelSNelson <85200392+MichaelSNelson@users.noreply.github.com>
2025-11-17 10:37:26 -0600

    Update AcquisitionManager.java

--- src/main/java/qupath/ext/qpsc/controller/workflow/AcquisitionManager.java
+++ src/main/java/qupath/ext/qpsc/controller/workflow/AcquisitionManager.java
@@ -38,6 +38,7 @@ import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
